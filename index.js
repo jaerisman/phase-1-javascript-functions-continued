1548a74d74a2d5c42f569b8bae667c29d7500aac
Jacob Erisman <jacob.erisman@gmail.com>
2023-08-29 10:04:58 -0400

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return (`This Saturday, I want to ${activity}!`);
+}
+saturdayFun();
+
+function mondayWork(work = "go to the office") {
+    return (`This Monday, I will ${work}.`);
+}
+mondayWork();
+
+function wrapAdjective(visualFlair="*") {
+    return function(adjective = "special"){
+        return(`You are ${visualFlair}${adjective}${visualFlair}!`);
+    }
+}
+wrapAdjective();
